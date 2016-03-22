9c38ec85379f8fe3005d50ca62ef3180596b51fe
tewilove <tewilove@gmail.com>
2016-03-22 14:28:10 +0800

    tested

--- jni/apps/hook_sharp_fota.c
+++ jni/apps/hook_sharp_fota.c
@@ -23,9 +23,67 @@ static void com_nttdocomo_android_fota_DmcIpl_iplRestoreFotaNetworkSettings_V(JN
     LOGV("emulate iplRestoreFotaNetworkSettings");
 }
 
+#if 1
+// isRooted() => false
+static jboolean com_nttdocomo_android_fota_DmcUtil_isRooted_Z(JNIEnv *env, jobject thiz) {
+    return 0;
+}
+#endif
+
+#if 0
+static jboolean java_io_File_exists_Z(JNIEnv *env, jobject thiz) {
+    jclass clz_File;
+    jmethodID mid_exists;
+    jfieldID fid_path;
+    jobject obj_path;
+    jboolean ret = 0;
+
+    LOGV("enter File.exists()");
+    if (thiz == NULL)
+        return 0;
+    clz_File = (*env)->GetObjectClass(env, thiz);
+    if (clz_File == NULL) {
+        if ((*env)->ExceptionOccurred(env))
+            (*env)->ExceptionClear(env);
+        return 0;
+    }
+    mid_exists = (*env)->GetMethodID(env, clz_File, "exists", "()Z");
+    ret = (*env)->CallBooleanMethod(env, thiz, mid_exists);
+    fid_path = (*env)->GetFieldID(env, clz_File, "path", "Ljava/lang/String;");
+    obj_path = (*env)->GetObjectField(env, thiz, fid_path);
+    if (obj_path) {
+        const char *cstr_path;
+
+        cstr_path = (*env)->GetStringUTFChars(env, obj_path, NULL);
+        if (cstr_path) {
+            LOGV("origin File.exists() returned %d", ret);
+            if (!strcmp(cstr_path, "/system/bin/su") ||
+                !strcmp(cstr_path, "/system/xbin/su"))
+                ret = 0;
+            (*env)->ReleaseStringUTFChars(env, obj_path, cstr_path);
+            LOGV("hooked File.exists() returned %d", ret);
+        }    
+    }
+    (*env)->DeleteLocalRef(env, obj_path);
+    (*env)->DeleteLocalRef(env, fid_path);
+    (*env)->DeleteLocalRef(env, mid_exists);
+    return ret;
+}
+#endif
+
+// return DATA_CONNECTED
+static jint android_telephony_TelephonyManager_getDataState_I(JNIEnv *env, jobject thiz) {
+    return 2;
+}
+
 // return SIM_STATE_READY
 static jint android_telephony_TelephonyManager_getSimState_I(JNIEnv *env, jobject thiz) {
     return 5;
+}
+
+// return false
+static jboolean android_telephony_TelephonyManager_isNetworkRoaming_Z(JNIEnv *env, jobject thiz) {
+    return 0;
 }
 
 // return DOCOMO
@@ -79,15 +137,20 @@ static jstring android_telephony_TelephonyManager_getSubscriberId_L_sbm(JNIEnv *
 }
 
 static struct hook_java_args gen_dcm_osv_stubs[] = {
+    { .clz = "android/telephony/TelephonyManager", .mtd = "getDataState", .sig = "()I", .func = android_telephony_TelephonyManager_getDataState_I },
+    { .clz = "android/telephony/TelephonyManager", .mtd = "isNetworkRoaming", .sig = "()Z", .func = android_telephony_TelephonyManager_isNetworkRoaming_Z },
     { .clz = "android/telephony/TelephonyManager", .mtd = "getLine1Number", .sig = "()Ljava/lang/String;", .func = android_telephony_TelephonyManager_getLine1Number_L_dcm },
     { .clz = "android/telephony/TelephonyManager", .mtd = "getNetworkOperator", .sig = "()Ljava/lang/String;", .func = android_telephony_TelephonyManager_getNetworkOperator_L_dcm },
     { .clz = "android/telephony/TelephonyManager", .mtd = "getSimOperator", .sig = "()Ljava/lang/String;", .func = android_telephony_TelephonyManager_getSimOperator_L_dcm},
     { .clz = "android/telephony/TelephonyManager", .mtd = "getSimState", .sig = "()I", .func = android_telephony_TelephonyManager_getSimState_I },
     { .clz = "android/telephony/TelephonyManager", .mtd = "getSubscriberId", .sig = "()Ljava/lang/String;", .func = android_telephony_TelephonyManager_getSubscriberId_L_dcm },
+    { .clz = "com/nttdocomo/android/osv/Utils", .mtd = "isRooted", .sig = "()Z", .func = com_nttdocomo_android_fota_DmcUtil_isRooted_Z },
     { .clz = 0 },
 };
 
 static struct hook_java_args gen_dcm_fota_stubs[] = {
+    { .clz = "android/telephony/TelephonyManager", .mtd = "getDataState", .sig = "()I", .func = android_telephony_TelephonyManager_getDataState_I },
+    { .clz = "android/telephony/TelephonyManager", .mtd = "isNetworkRoaming", .sig = "()Z", .func = android_telephony_TelephonyManager_isNetworkRoaming_Z },
     { .clz = "android/telephony/TelephonyManager", .mtd = "getLine1Number", .sig = "()Ljava/lang/String;", .func = android_telephony_TelephonyManager_getLine1Number_L_dcm },
     { .clz = "android/telephony/TelephonyManager", .mtd = "getNetworkOperator", .sig = "()Ljava/lang/String;", .func = android_telephony_TelephonyManager_getNetworkOperator_L_dcm },
     { .clz = "android/telephony/TelephonyManager", .mtd = "getSimOperator", .sig = "()Ljava/lang/String;", .func = android_telephony_TelephonyManager_getSimOperator_L_dcm},
@@ -96,10 +159,13 @@ static struct hook_java_args gen_dcm_fota_stubs[] = {
     { .clz = "com/nttdocomo/android/fota/DmcIpl", .mtd = "iplGetFotaNetworkSettings", .sig = "()I", .func = com_nttdocomo_android_fota_DmcIpl_iplGetFotaNetworkSettings_I },
     { .clz = "com/nttdocomo/android/fota/DmcIpl", .mtd = "iplSetFotaNetworkSettings", .sig = "()I", .func = com_nttdocomo_android_fota_DmcIpl_iplSetFotaNetworkSettings_I },
     { .clz = "com/nttdocomo/android/fota/DmcIpl", .mtd = "iplRestoreFotaNetworkSettings", .sig = "()V", .func = com_nttdocomo_android_fota_DmcIpl_iplRestoreFotaNetworkSettings_V },
+    { .clz = "com/nttdocomo/android/fota/DmcUtil", .mtd = "isRooted", .sig = "()Z", .func = com_nttdocomo_android_fota_DmcUtil_isRooted_Z },
     { .clz = 0 },
 };
 
 static struct hook_java_args gen_sbm_fota_stubs[] = {
+    { .clz = "android/telephony/TelephonyManager", .mtd = "getDataState", .sig = "()I", .func = android_telephony_TelephonyManager_getDataState_I },
+    { .clz = "android/telephony/TelephonyManager", .mtd = "isNetworkRoaming", .sig = "()Z", .func = android_telephony_TelephonyManager_isNetworkRoaming_Z },
     { .clz = "android/telephony/TelephonyManager", .mtd = "getLine1Number", .sig = "()Ljava/lang/String;", .func = android_telephony_TelephonyManager_getLine1Number_L_sbm },
     { .clz = "android/telephony/TelephonyManager", .mtd = "getNetworkOperator", .sig = "()Ljava/lang/String;", .func = android_telephony_TelephonyManager_getNetworkOperator_L_sbm },
     { .clz = "android/telephony/TelephonyManager", .mtd = "getSimOperator", .sig = "()Ljava/lang/String;", .func = android_telephony_TelephonyManager_getSimOperator_L_sbm},
@@ -117,15 +183,29 @@ static struct sharp_model_info mtab_fota[] = {
     { .model = "SBM302SH", .stubs = gen_sbm_fota_stubs },
     { .model = "SBM303SH", .stubs = gen_sbm_fota_stubs },
     { .model = "304SH", .stubs = gen_sbm_fota_stubs },
+    { .model = "305SH", .stubs = gen_sbm_fota_stubs },
+    { .model = "402SH", .stubs = gen_sbm_fota_stubs },
     { .model = "SH-06E", .stubs = gen_dcm_fota_stubs },
     { .model = "SH-01F", .stubs = gen_dcm_fota_stubs },
+    { .model = "SH-02F", .stubs = gen_dcm_fota_stubs },
+    { .model = "SH-03F", .stubs = gen_dcm_fota_stubs },
     { .model = "SH-04F", .stubs = gen_dcm_fota_stubs },
+    { .model = "SH-05F", .stubs = gen_dcm_fota_stubs },
+    { .model = "SH-06F", .stubs = gen_dcm_fota_stubs },
+    { .model = "SH-01G", .stubs = gen_dcm_fota_stubs },
+    { .model = "SH-02G", .stubs = gen_dcm_fota_stubs },
 };
 
 static struct sharp_model_info mtab_osv[] = {
     { .model = "SH-06E", .stubs = gen_dcm_osv_stubs },
     { .model = "SH-01F", .stubs = gen_dcm_osv_stubs },
+    { .model = "SH-02F", .stubs = gen_dcm_osv_stubs },
+    { .model = "SH-03F", .stubs = gen_dcm_osv_stubs },
     { .model = "SH-04F", .stubs = gen_dcm_osv_stubs },
+    { .model = "SH-05F", .stubs = gen_dcm_osv_stubs },
+    { .model = "SH-06F", .stubs = gen_dcm_osv_stubs },
+    { .model = "SH-01G", .stubs = gen_dcm_osv_stubs },
+    { .model = "SH-02G", .stubs = gen_dcm_osv_stubs },
 };
 
 int SharpMain(int argc, char *argv[]) {
@@ -139,7 +219,8 @@ int SharpMain(int argc, char *argv[]) {
         return -1;
     }
     if (!strcmp(argv[0], "com.nttdocomo.android.fota") ||
-        !strcmp(argv[0], "jp.co.sharp.android.FotaApp")) {
+        !strcmp(argv[0], "jp.co.sharp.android.FotaApp") ||
+        !strcmp(argv[0], "com.redbend.dmClient")) {
         mtab = mtab_fota;
         n = sizeof(mtab_fota) / sizeof(*mtab);
     } else if (!strcmp(argv[0], "com.nttdocomo.android.osv")) {
@@ -157,19 +238,21 @@ int SharpMain(int argc, char *argv[]) {
     property_get("ro.product.model", model, "");
     for (i = 0; i < n; i++) {
         struct sharp_model_info *info = &mtab[i];
+        int ret = 0;
 
         if (!strcmp(info->model, model)) {
             struct hook_java_args *args;
 
             for (args = info->stubs; args->clz; args++) {
                 rc = hook_java(env, args);
+                ret |= rc;
                 if (rc) {
                     LOGE("hook_java failed: L%s;->%s%s", args->clz, args->mtd, args->sig);
-                    break;
+                    continue;
                 }
             }
 
-            return rc;
+            return ret;
         }
     }
 
